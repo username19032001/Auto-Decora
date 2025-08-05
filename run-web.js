8968d18232ac0cc62866a04125c6c67de509ac72
username19032001 <testsafd@gmail.com>
2025-08-05 18:16:30 +0530

    Delete run-web.js

--- run-web.js
+++
@@ -1,9 +0,0 @@
-const { run } = require("turbo");
-
-run(["run", "dev", "--filter=web"])
-  .then(() => {
-    console.log("Turbo web dev started!");
-  })
-  .catch((err) => {
-    console.error("Failed to start web:", err);
-  });
