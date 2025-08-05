a1810912c736aae2da1aff47d8054d486d2d098f
username19032001 <testsafd@gmail.com>
2025-08-05 18:30:40 +0530

    Update and rename run-web.cjs to run-web.js

---
+++ run-web.js
@@ -0,0 +1,13 @@
+const { exec } = require("child_process");
+
+exec("npx turbo run dev --filter=web", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+  console.log(stdout);
+});
